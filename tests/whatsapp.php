a46f5b681c250038ab65c401f5639226b12a303f
rapji13 <raphiii98@gmail.com>
2013-09-04 18:05:19 +0200

    Update whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -17,8 +17,8 @@ function fgets_u($pStdn)
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc)
+$sender = 	""; 01606674996
+$imei = 	""; 192.3.75.145
 $password =     ""; // Password you received from WhatsApp
 
 if ($argc < 2) {
